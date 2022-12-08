1e08184ddac081e885fee9dd81975833f500f38a
Collins Mwambi <77346740+CollinsMwambi@users.noreply.github.com>
2022-12-08 14:47:15 +0300

    landing page modification

--- welcome.php
+++ welcome.php
@@ -6,9 +6,31 @@
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Hugo 0.87.0">
-  
+    <meta charset="utf-8">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.104.2">
+   
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/heroes/">
+
+    
+
+    
+
+<link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/heroes/">
+
+    
+
+    
+
+<link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
-    <title>Welcome to Eshara</title>
+    <title>Eshara</title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/carousel/">
 
@@ -31,56 +53,6 @@
           font-size: 3.5rem;
         }
       }
-    </style>
-
-    
-    <!-- Custom styles for this template -->
-    <link href="carousel.css" rel="stylesheet">
-  </head>
-  <body>
-    
-<header>
-  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
-    <div class="container-fluid">
-      <a class="navbar-brand" >Eshara</a>
-      
-      
-        <form class="d-flex">
-        <a class="nav-link" href="login.php">Already a member? Login</a>
-         
-        </form>
-      </div>
-    </div>
-  </nav>
-</header>
-
-<main>
-
- 
-        
-
-
-  
-
-    <!-- Three columns of text below the carousel -->
-    
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Hugo 0.104.2">
-    <title>Heroes · Bootstrap v5.2</title>
-
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/heroes/">
-
-    
-
-    
-
-<link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
-
-    <style>
       .bd-placeholder-img {
         font-size: 1.125rem;
         text-anchor: middle;
@@ -135,8 +107,26 @@
 
     
     <!-- Custom styles for this template -->
+    <link href="carousel.css" rel="stylesheet">
     <link href="heroes.css" rel="stylesheet">
   </head>
+  <body>
+    
+<header>
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" >Eshara</a>
+      
+      
+        <form class="d-flex">
+        <a class="nav-link" href="login.php">Already a member? Login</a>
+         
+        </form>
+      </div>
+    </div>
+  </nav>
+</header>
+  
   <body>
     
 <main>
